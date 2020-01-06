77f5b4b56b195bd0bd8cdca5aca4438b7947f252
Rahul Gopinath <rahul@gopinath.org>
2020-01-06 19:57:12 +0100

    add more rules

--- tofuzzingbook.py
+++ tofuzzingbook.py
@@ -16,30 +16,40 @@ class AntlrG:
         self.tree = self.parser.grammarSpec() # entry
         self.res = self.parse_grammarSpec(self.tree)
 
+    def parse_token(self, children, typ):
+        assert isinstance(children[0], tree.Tree.TerminalNodeImpl)
+        tok = children.pop(0)
+        assert tok.symbol.type == typ
+        return tok, children
+
     def parse_object(self, children, typ):
         assert isinstance(children[0], typ)
         obj = children.pop(0)
         return obj, children
 
-    def parse_objectList(self, children, typ):
+    def parse_objectList(self, children, typ, n=-1):
         lst = []
         while children:
+            if n == 0: break
+            n -= 1
             if isinstance(children[0], typ):
-                res = children.pop(0)
-                assert isinstance(res, typ)
-                lst.append(res)
+                obj = children.pop(0)
+                assert isinstance(obj, typ)
+                lst.append(obj)
             else:
                 break
         return lst, children
 
-    def parse_tokenList(self, children, typ):
+    def parse_tokenList(self, children, typ, n=-1):
         lst = []
         while children:
+            if n == 0: break
+            n -= 1
             if isinstance(children[0], tree.Tree.TerminalNodeImpl):
                 if children[0].symbol.type != typ: break
-                res = children.pop(0)
-                assert res.symbol.type == typ 
-                lst.append(res)
+                tok = children.pop(0)
+                assert tok.symbol.type == typ
+                lst.append(tok)
             else:
                 break
         return lst, children
@@ -67,7 +77,7 @@ class AntlrG:
         _o, children = self.parse_objectList(children, ANTLRv4Parser.ModeSpecContext)
         self.parse_modeSpec_star(_o)
 
-        _o, children = self.parse_object(children, ANTLRv4Parser.EOF)
+        _o, children = self.parse_token(children, self.parser.EOF)
         self.parse_EOF(_o)
         return rules_json
         #tree.toStringTree(recog=self.parser)
@@ -103,10 +113,12 @@ class AntlrG:
         return None
 
     def parse_rulesSpec_star(self, ruleSpec_star):
+        rules_json = []
         for rule in ruleSpec_star:
-            self.parse_ruleSpec(rule)
+            o = self.parse_ruleSpec(rule)
+            rules_json.append(o)
 
-        return rules_json, children
+        return rules_json
 
     def parse_ruleSpec(self, ruleSpec):
         '''
@@ -131,7 +143,54 @@ class AntlrG:
            : DOC_COMMENT* ruleModifiers? RULE_REF argActionBlock? ruleReturns? throwsSpec? localsSpec? rulePrequel* COLON ruleBlock SEMI exceptionGroup
            ;
         '''
-        return rspec
+        children = copy.copy(rspec.children)
+        # parse and ignore
+        _o, children = self.parse_tokenList(children, self.lexer.DOC_COMMENT)
+        self.parse_DOC_COMMENT_star(_o)
+
+        _o, children = self.parse_objectList(children, ANTLRv4Parser.RuleModifiersContext, n=1) # a maximum of one
+        self.parse_ruleModifiers_question(_o)
+
+        _o, children = self.parse_token(children, self.lexer.RULE_REF)
+        rule_name = self.parse_RULE_REF(_o)
+
+        return [rule_name, []]
+
+    def parse_RULE_REF(self, obj): return obj.symbol.text
+
+    def parse_TOKEN_REF(self, obj): return obj.symbol.text
+
+    def parse_ruleModifiers_question(self, objs):
+        if not objs: return []
+        assert len(objs) == 1
+        return self.parse_ruleModifier(objs[0])
+
+    def parse_ruleModifier(self, obj):
+        '''
+        ruleModifier
+           : PUBLIC
+           | PRIVATE
+           | PROTECTED
+           | FRAGMENT
+           ;
+        '''
+        assert len(obj.children) == 1
+        c = obj.children[0]
+        if c.symbol.type == self.lexer.FRAGMENT: return self.parse_FRAGMENT(c)
+        elif c.symbol.type == self.lexer.PRIVATE: return self.parse_PRIVATE(c)
+        elif c.symbol.type == self.lexer.PUBLIC: return self.parse_PUBLIC(c)
+        elif c.symbol.type == self.lexer.PROTECTED: return self.parse_PROTECTED(c)
+        else: assert FALSE
+
+    def parse_FRAGMENT(self, obj): return obj.symbol.text
+    def parse_PUBLIC(self, obj): return obj.symbol.text
+    def parse_PRIVATE(self, obj): return obj.symbol.text
+    def parse_PROTECTED(self, obj): return obj.symbol.text
+
+    def parse_FRAGMENT_question(self, objs):
+        if not objs: return []
+        assert len(objs) == 1
+        return self.parse_FRAGMENT(objs[0])
 
     def parse_lexerRuleSpec(self, rspec):
         '''
@@ -139,7 +198,23 @@ class AntlrG:
            : DOC_COMMENT* FRAGMENT? TOKEN_REF COLON lexerRuleBlock SEMI
            ;
         '''
-        return rspec
+        children = rspec.children
+        _o, children = self.parse_tokenList(children, self.lexer.DOC_COMMENT)
+        self.parse_DOC_COMMENT_star(_o)
+
+        _o, children = self.parse_tokenList(children, self.lexer.FRAGMENT, n=1)
+        self.parse_FRAGMENT_question(_o)
+
+        _o, children = self.parse_token(children, self.lexer.TOKEN_REF)
+        token_name = self.parse_TOKEN_REF(_o)
+
+        _o, children = self.parse_token(children, self.lexer.COLON)
+
+        _o, children = self.parse_object(children, ANTLRv4Parser.LexerRuleBlockContext)
+
+        _o, children = self.parse_token(children, self.lexer.SEMI)
+
+        return [token_name, []]
 
     def parse_rules(self, rules):
         '''
@@ -149,7 +224,7 @@ class AntlrG:
         '''
         ruleSpec_star, children = self.parse_objectList(copy.copy(rules.children), ANTLRv4Parser.RuleSpecContext)
         rules_json = self.parse_rulesSpec_star(ruleSpec_star)
-        return rules_json, children
+        return rules_json
 
 def main():
     with open(sys.argv[1], 'r') as f:
