40f16e8988157009c4bf233003b9ab97a8bd9a97
Rahul Gopinath <rahul@gopinath.org>
2020-01-06 17:33:41 +0100

    Init

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+
+all:
+	java -Xmx500M -cp ../antlr-4.7.2-complete.jar org.antlr.v4.Tool -Dlanguage=Python3 ANTLRv4Lexer.g4
+	java -Xmx500M -cp ../antlr-4.7.2-complete.jar org.antlr.v4.Tool -Dlanguage=Python3 ANTLRv4Parser.g4
+
+prereq:
+	wget https://www.antlr.org/download/antlr-4.7.2-complete.jar
+	mv antlr-4.7.2-complete.jar ..
+	pip install wheel
+	pip install antlr4-python3-runtime
---
+++ tofuzzingbook.py
@@ -0,0 +1,161 @@
+from antlr4 import *
+from ANTLRv4Lexer import ANTLRv4Lexer
+from ANTLRv4Parser import ANTLRv4Parser
+import sys
+import copy
+
+
+class AntlrG:
+    def __init__(self, code):
+        self.lexer = ANTLRv4Lexer(InputStream(code))
+
+        tokenStream = CommonTokenStream(self.lexer)
+        self.parser = ANTLRv4Parser(tokenStream)
+        self.parser.buildParseTrees = True
+
+        self.tree = self.parser.grammarSpec() # entry
+        self.res = self.parse_grammarSpec(self.tree)
+
+    def parse_object(self, children, typ):
+        assert isinstance(children[0], typ)
+        obj = children.pop(0)
+        return obj, children
+
+    def parse_objectList(self, children, typ):
+        lst = []
+        while children:
+            if isinstance(children[0], typ):
+                res = children.pop(0)
+                assert isinstance(res, typ)
+                lst.append(res)
+            else:
+                break
+        return lst, children
+
+    def parse_tokenList(self, children, typ):
+        lst = []
+        while children:
+            if isinstance(children[0], tree.Tree.TerminalNodeImpl):
+                if children[0].symbol.type != typ: break
+                res = children.pop(0)
+                assert res.symbol.type == typ 
+                lst.append(res)
+            else:
+                break
+        return lst, children
+
+    def parse_grammarSpec(self, parsedtree):
+        '''
+        grammarSpec
+           : DOC_COMMENT* grammarDecl prequelConstruct* rules modeSpec* EOF
+           ;
+        '''
+        children = copy.copy(parsedtree.children)
+        # if there are doc_comments, skip them first
+        _o, children = self.parse_tokenList(children, self.lexer.DOC_COMMENT)
+        self.parse_DOC_COMMENT_star(_o)
+
+        _o, children = self.parse_object(children, ANTLRv4Parser.GrammarDeclContext)
+        self.parse_grammarDecl(_o)
+
+        _o, children = self.parse_objectList(children, ANTLRv4Parser.PrequelConstructContext)
+        self.parse_prequelConstruct_star(_o)
+
+        rules, children = self.parse_object(children, ANTLRv4Parser.RulesContext)
+        rules_json = self.parse_rules(rules)
+
+        _o, children = self.parse_objectList(children, ANTLRv4Parser.ModeSpecContext)
+        self.parse_modeSpec_star(_o)
+
+        _o, children = self.parse_object(children, ANTLRv4Parser.EOF)
+        self.parse_EOF(_o)
+        return rules_json
+        #tree.toStringTree(recog=self.parser)
+
+
+    def parse_EOF(self, children): return None
+
+    def parse_prequelConstruct_star(self, children): return None
+
+    def parse_prequelConstruct(self, children):
+        '''
+        prequelConstruct
+           : optionsSpec
+           | delegateGrammars
+           | tokensSpec
+           | channelsSpec
+           | action_
+           ;
+        '''
+        ...
+
+    def parse_DOC_COMMENT_star(self, o): return None
+
+    def parse_modeSpec_star(self, children): return None
+
+    def parse_grammarDecl(self, children):
+        # SKIPPED
+        '''
+        grammarDecl
+           : grammarType identifier SEMI
+           ;
+        '''
+        return None
+
+    def parse_rulesSpec_star(self, ruleSpec_star):
+        for rule in ruleSpec_star:
+            self.parse_ruleSpec(rule)
+
+        return rules_json, children
+
+    def parse_ruleSpec(self, ruleSpec):
+        '''
+        ruleSpec
+           : parserRuleSpec
+           | lexerRuleSpec
+           ;
+        '''
+        children = ruleSpec.children
+        assert len(children) == 1
+        rspec = children[0]
+        if isinstance(rspec, ANTLRv4Parser.ParserRuleSpecContext):
+            return self.parse_parserRuleSpec(rspec)
+        elif isinstance(rspec, ANTLRv4Parser.LexerRuleSpecContext):
+            return self.parse_lexerRuleSpec(rspec)
+        else:
+            assert False
+
+    def parse_parserRuleSpec(self, rspec):
+        '''
+        parserRuleSpec
+           : DOC_COMMENT* ruleModifiers? RULE_REF argActionBlock? ruleReturns? throwsSpec? localsSpec? rulePrequel* COLON ruleBlock SEMI exceptionGroup
+           ;
+        '''
+        return rspec
+
+    def parse_lexerRuleSpec(self, rspec):
+        '''
+        lexerRuleSpec
+           : DOC_COMMENT* FRAGMENT? TOKEN_REF COLON lexerRuleBlock SEMI
+           ;
+        '''
+        return rspec
+
+    def parse_rules(self, rules):
+        '''
+        rules
+           : ruleSpec*
+           ;
+        '''
+        ruleSpec_star, children = self.parse_objectList(copy.copy(rules.children), ANTLRv4Parser.RuleSpecContext)
+        rules_json = self.parse_rulesSpec_star(ruleSpec_star)
+        return rules_json, children
+
+def main():
+    with open(sys.argv[1], 'r') as f:
+        code = f.read()
+    ag = AntlrG(code)
+    print(ag.res)
+
+if __name__ == '__main__':
+    main()
