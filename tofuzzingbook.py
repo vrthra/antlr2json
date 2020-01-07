90e374a4bf3551fe9e429c1ee8d19b819dfa6570
Rahul Gopinath <rahul@gopinath.org>
2020-01-07 10:21:36 +0100

    most of lexing

--- tofuzzingbook.py
+++ tofuzzingbook.py
@@ -106,12 +106,30 @@ class AntlrG:
         return rules_json
         #tree.toStringTree(recog=self.parser)
 
+    def parse_LEXER_CHAR_SET(self, obj):
+        return obj.symbol.text
 
-    def parse_EOF(self, children): return None
+    def parse_NOT(self, obj):
+        assert obj.symbol.text == '~'
+        return None
 
-    def parse_prequelConstruct_star(self, children): return None
+    def parse_RARROW(self, obj):
+        assert obj.symbol.text == '->'
+        return None
 
-    def parse_prequelConstruct(self, children):
+    def parse_LPAREN(self, obj):
+        assert obj.symbol.text == '('
+        return None
+
+    def parse_RPAREN(self, obj):
+        assert obj.symbol.text == ')'
+        return None
+
+    def parse_EOF(self, obj): return None
+
+    def parse_prequelConstruct_star(self, obj): return None
+
+    def parse_prequelConstruct(self, obj):
         '''
         prequelConstruct
            : optionsSpec
@@ -121,7 +139,7 @@ class AntlrG:
            | action_
            ;
         '''
-        ...
+        return None
 
     def parse_DOC_COMMENT_star(self, o): return None
 
@@ -316,6 +334,36 @@ class AntlrG:
         else:
             assert False
 
+    def parse_setElement(self, obj):
+        '''
+        setElement
+           : TOKEN_REF elementOptions?
+           | STRING_LITERAL elementOptions?
+           | characterRange
+           | LEXER_CHAR_SET
+           ;
+        '''
+        children = copy.copy(obj.children)
+        c = children.pop(0)
+        if isinstance(c, tree.Tree.TerminalNodeImpl):
+            if c.symbol.type == self.lexer.TOKEN_REF:
+                assert not children
+                return self.parse_TOKEN_REF(c)
+            elif c.symbol.type == self.lexer.STRING_LITERAL:
+                assert not children
+                return self.parse_STRING_LITERAL(c)
+            elif c.symbol.type == self.lexer.LEXER_CHAR_SET:
+                assert not children
+                return self.parse_LEXER_CHAR_SET(c)
+            else:
+                assert False
+
+        elif isinstance(c, ANTLRv4Parser.CharacterRangeContext):
+            assert not children
+            return self.parse_characterRange(c)
+        else:
+            assert False
+
     def parse_ebnf(self, obj):
         '''
         ebnf
@@ -396,6 +444,26 @@ class AntlrG:
         else:
             assert isinstance(c, tree.Tree.TerminalNodeImpl)
             raise NotImplemented()
+
+    def parse_notSet(self, obj):
+        '''
+        notSet
+           : NOT setElement
+           | NOT blockSet
+           ;
+        '''
+        children = copy.copy(obj.children)
+        _o, children = self._parse_token(children, self.lexer.NOT)
+        self.parse_NOT(_o)
+        c = children.pop(0)
+        assert not children
+        if isinstance(c, ANTLRv4Parser.SetElementContext):
+            v = self.parse_setElement(c)
+            return ('not', v)
+        elif isinstance(c, ANTLRv4Parser.BlockSetContext):
+            v = self.parse_blockSet(c)
+            return ('not', v)
+        else: assert False
 
     def parse_terminal(self, obj):
         '''
@@ -485,7 +553,9 @@ class AntlrG:
         v = obj.symbol.text
         return '<%s>' % v
 
-    def parse_TOKEN_REF(self, obj): return obj.symbol.text
+    def parse_TOKEN_REF(self, obj):
+        v = obj.symbol.text
+        return '<%s>' % v
 
     def parse_ruleModifiers_question(self, objs):
         if not objs: return []
@@ -538,10 +608,239 @@ class AntlrG:
         _o, children = self._parse_token(children, self.lexer.COLON)
 
         _o, children = self._parse_object(children, ANTLRv4Parser.LexerRuleBlockContext)
+        tdef = self.parse_lexerRuleBlock(_o)
 
         _o, children = self._parse_token(children, self.lexer.SEMI)
 
-        return [token_name, []]
+        return [token_name, tdef]
+
+    def parse_lexerRuleBlock(self, obj):
+        '''
+        lexerRuleBlock
+           : lexerAltList
+           ;
+        '''
+        altlst, children = self._parse_object(copy.copy(obj.children), ANTLRv4Parser.LexerAltListContext)
+        assert not children
+        res = self.parse_lexerAltList(altlst)
+        return res
+
+    def parse_lexerAltList(self, obj):
+        '''
+        lexerAltList
+           : lexerAlt (OR lexerAlt)*
+           ;
+        '''
+        # a define with multiple rules
+        def pred_inside(children):
+            _o, children = self._parse_question_token(children, self.lexer.OR)
+            if _o is None: return None
+            _o, children = self._parse_object(children, ANTLRv4Parser.LexerAltContext)
+            assert _o is not None
+            return _o, children
+
+        children = copy.copy(obj.children)
+        o = children.pop(0)
+        lalt_children = [o]
+
+        cs, children = self._parse_star_x(children, pred_inside)
+        lalt_children.extend(cs)
+        res = []
+        for c in lalt_children:
+            o = self.parse_lexerAlt(c)
+            res.append(o)
+        return res
+
+    def parse_lexerAlt(self, obj):
+        '''
+        lexerAlt
+           : lexerElements lexerCommands?
+           |
+           // explicitly allow empty alts
+           ;
+        '''
+        cs = copy.copy(obj.children)
+        if not cs: return []
+        _o, children = self._parse_object(cs, ANTLRv4Parser.LexerElementsContext)
+        v = self.parse_lexerElements(_o)
+        _o, children = self._parse_question_object(cs, ANTLRv4Parser.LexerCommandsContext)
+        if _o:
+            self.parse_lexerCommands(_o[0])
+        return v
+
+    def parse_lexerCommands(self, obj):
+        '''
+        lexerCommands
+           : RARROW lexerCommand (COMMA lexerCommand)*
+           ;
+        '''
+        # a single production rule
+        def pred_inside(children_):
+            _o, children = self._parse_question_token(children, self.lexer.COMMA)
+            if _o is None: return None
+            _o, children = self._parse_object(children, ANTLRv4Parser.LexerCommandContext)
+            assert _o is not None
+            return _o
+
+        children = copy.copy(obj.children)
+        _o, children = self._parse_token(children, self.lexer.RARROW)
+        self.parse_RARROW(_o)
+        _o, children = self._parse_object(children, ANTLRv4Parser.LexerCommandContext)
+        lcommands = [_o]
+
+        res, children = self._parse_star_x(children, pred_inside)
+        lcommands.extend(res)
+
+        res = []
+        for c in lcommands:
+            v = self.parse_lexerCommand(c)
+            res.append(v)
+        return res
+
+    def parse_lexerCommand(self, obj):
+        '''
+        lexerCommand
+           : lexerCommandName LPAREN lexerCommandExpr RPAREN
+           | lexerCommandName
+           ;
+        '''
+        children = copy.copy(obj.children)
+        cn, children = self._parse_object(children, ANTLRv4Parser.LexerCommandNameContext)
+        cname = self.parse_lexerCommandName(cn)
+        if not children:
+            return cname
+
+        assert False
+        _o, children = self._parse_token(children, self.lexer.LPAREN)
+        self.parse_LPAREN(_o)
+        ce, children = self._parse_object(children, ANTLRv4Parser.LexerCommandExprContext)
+        _o, children = self._parse_token(children, self.lexer.RPAREN)
+        self.parse_RPAREN(_o)
+        assert not children
+
+        cname = self.parse_lexerCommandName(cn)
+        cexp = self.parse_lexerCommandExpr(ce)
+        return (cname, cexp)
+
+    def parse_lexerCommandName(self, obj):
+        '''
+        lexerCommandName
+           : identifier
+           | MODE
+           ;
+        '''
+        children = copy.copy(obj.children)
+        c = children[0]
+        if isinstance(c, ANTLRv4Parser.IdentifierContext):
+            _o, children = self._parse_object(children, ANTLRv4Parser.IdentifierContext)
+            assert not children
+            return self.parse_identifier(_o)
+        elif isinstance(c, tree.Tree.TerminalNodeImpl):
+            _o, children = self._parse_token(children, self.lexer.MODE)
+            assert not children
+            return self.parse_MODE(_o)
+        else:
+            assert False
+
+
+    def parse_lexerCommandExpr(self, obj):
+        '''
+        lexerCommandExpr
+           : identifier
+           | INT
+           ;
+           // --------------------
+           // Rule Alts
+        '''
+        raise NotImplemented()
+
+    def parse_lexerElements(self, obj):
+        '''
+        lexerElements
+           : lexerElement+
+           ;
+        '''
+        _o, children = self._parse_star_object(copy.copy(obj.children), ANTLRv4Parser.LexerElementContext)
+        assert len(_o) > 0 # plus
+        res = []
+        for val in _o:
+            o = self.parse_lexerElement(val)
+            res.append(o)
+        return res
+
+    def parse_lexerElement(self, obj):
+        '''
+        lexerElement
+           : labeledLexerElement ebnfSuffix?
+           | lexerAtom ebnfSuffix?
+           | lexerBlock ebnfSuffix?
+           | actionBlock QUESTION?
+           ;
+        '''
+        children = copy.copy(obj.children)
+        c = children.pop(0)
+        if isinstance(c, ANTLRv4Parser.LabeledLexerElementContext):
+            assert not children
+            return self.parse_labeledLexerElement(c)
+        elif isinstance(c, ANTLRv4Parser.LexerAtomContext):
+            ebnf_suffix, children = self._parse_question_object(children, ANTLRv4Parser.EbnfSuffixContext)
+            assert not children
+            ebnf = None if not ebnf_suffix else [self.parse_ebnfSuffix(ebnf_suffix[0])]
+            res = self.parse_lexerAtom(c)
+            return (res, ebnf)
+        elif isinstance(c, ANTLRv4Parser.ActionBlockContext):
+            assert not children
+            return self.parse_actionBlock(c)
+        elif isinstance(c, ANTLRv4Parser.LexerBlockContext):
+            ebnf_suffix, children = self._parse_question_object(children, ANTLRv4Parser.EbnfSuffixContext)
+            assert not children
+            res = self.parse_lexerBlock(c)
+            ebnf = None if not ebnf_suffix else [self.parse_ebnfSuffix(ebnf_suffix[0])]
+            return (res, ebnf)
+        else: assert False
+
+    def parse_lexerBlock(self, obj):
+        '''
+        lexerBlock
+           : LPAREN lexerAltList RPAREN
+           ;
+        '''
+        children = copy.copy(obj.children)
+        lparen, children = self._parse_token(children, self.lexer.LPAREN)
+        self.parse_LPAREN(lparen)
+        lalts, children = self._parse_object(children, ANTLRv4Parser.LexerAltListContext)
+        res = self.parse_lexerAltList(lalts)
+        rparen, children = self._parse_token(children, self.lexer.RPAREN)
+        self.parse_RPAREN(rparen)
+        assert not children
+        return res
+
+    def parse_lexerAtom(self, obj):
+        '''
+        lexerAtom
+           : characterRange
+           | terminal
+           | notSet
+           | LEXER_CHAR_SET
+           | DOT elementOptions?
+           ;
+        '''
+        children = copy.copy(obj.children)
+        c = children.pop(0)
+        assert not children
+        if isinstance(c, ANTLRv4Parser.CharacterRangeContext):
+            return self.parse_characterRange(c)
+        elif isinstance(c, ANTLRv4Parser.TerminalContext):
+            return self.parse_terminal(c)
+        elif isinstance(c, ANTLRv4Parser.NotSetContext):
+            return self.parse_notSet(c)
+        elif isinstance(c, tree.Tree.TerminalNodeImpl):
+            if c.symbol.type == self.lexer.LEXER_CHAR_SET:
+                return self.parse_LEXER_CHAR_SET(c)
+            elif c.symbol.type == self.lexer.DOT:
+                assert False
+        else:
+            assert False
 
     def parse_rules(self, rules):
         '''
