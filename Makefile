0e07988cf722560a4eb7307eae51fb54b78230f1
Rahul Gopinath <rahul@gopinath.org>
2020-01-07 20:08:40 +0100

    update

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 JSON.fbjson: JSON.ebnf
-	python3 ebnftosimple.py JSON.ebnf | tee JSON.fbjson
+	python3 ebnftosimple.py JSON.ebnf > JSON.fbjson
+	python3 show.py JSON.fbjson
 
 JSON.ebnf: examples/JSON.g4 ANTLRv4Lexer.py ANTLRv4Parser.py
-	python3 tojson.py examples/JSON.g4 | tee JSON.ebnf
+	python3 tojson.py examples/JSON.g4 > JSON.ebnf
+	python3 show.py JSON.ebnf
 
 ANTLRv4Lexer.py: ANTLRv4Lexer.g4
 	java -Xmx500M -cp ../antlr-4.7.2-complete.jar org.antlr.v4.Tool -Dlanguage=Python3 ANTLRv4Lexer.g4
--- ebnftosimple.py
+++ ebnftosimple.py
@@ -74,6 +74,25 @@ def process_SEQ(regex, jval):
     jval[k] = [res]
     return k
 
+def process_CHARSET(val, jval):
+    #now get everything until the first range
+    v = val.find('-')
+    if v == -1:
+        return process_chars(val)
+    else:
+        if v == len(val) -1: # last is not special char
+            return process_chars(val)
+        else:
+            first_part = val[:v-1] # v-1 is the start char of range
+            # v + 1 is the end char of range
+            return process_chars(first_part) + process_range(val[v-1], val[v+1]) + process_CHARSET(val[v+2:], jval)
+
+def process_chars(chars):
+    return '[%s]' % chars
+
+def process_range(a, b):
+    return '[%s-%s]' % (a, b)
+
 def process_re(regex, jval):
     # return of process_re will be a token
     if isinstance(regex, str): return regex
@@ -92,6 +111,10 @@ def process_re(regex, jval):
             s = process_OR(val, jval)
         elif op == 'not':
             s = process_NOT(val, jval)
+        elif op == 'charset':
+            assert (val[0], val[-1]) == ('[', ']')
+            v = bytes(val[1:-1], 'utf-8').decode('unicode_escape')
+            s = process_CHARSET(v, jval)
         else:
             # this is a seq.
             s = process_SEQ(regex, jval)
