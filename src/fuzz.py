cce3943b82db5c4d6296fbbee582c8d844a4d687
Rahul Gopinath <rahul@gopinath.org>
2020-01-08 15:33:23 +0100

    limitfuzzer

--- src/fuzz.py
+++ src/fuzz.py
@@ -1,5 +1,57 @@
 import random
 
+class Fuzzer:
+    def __init__(self, grammar):
+        self.grammar = grammar
+
+    def fuzz(self, key='<start>', max_num=None, max_depth=None):
+        raise NotImplemented()
+
+class LimitFuzzer(Fuzzer):
+    def __init__(self, grammar):
+        super().__init__(grammar)
+        self.key_cost = {}
+        self.cost = self.compute_cost(grammar)
+
+    def compute_cost(self, grammar):
+        cost = {}
+        for k in grammar:
+            cost[k] = {}
+            for rule in grammar[k]:
+                cost[k][str(rule)] = self.expansion_cost(grammar, rule, set())
+        return cost
+
+
+    def symbol_cost(self, grammar, symbol, seen):
+        if symbol in self.key_cost: return self.key_cost[symbol]
+        if symbol in seen:
+            self.key_cost[symbol] = float('inf')
+            return float('inf')
+        v = min((self.expansion_cost(grammar, rule, seen | {symbol})
+                    for rule in grammar.get(symbol, [])), default=0)
+        self.key_cost[symbol] = v
+        return v
+
+    def expansion_cost(self, grammar, tokens, seen):
+        return max((self.symbol_cost(grammar, token, seen)
+                    for token in tokens if token in grammar), default=0) + 1
+
+    def gen_key(self, key, depth, max_depth):
+        if key not in self.grammar: return key
+        if depth > max_depth:
+            clst = sorted([(self.cost[key][str(rule)], rule) for rule in self.grammar[key]])
+            rules = [r for c,r in clst if c == clst[0][0]]
+        else:
+            rules = self.grammar[key]
+        return self.gen_rule(random.choice(rules), depth+1, max_depth)
+
+    def gen_rule(self, rule, depth, max_depth):
+        return ''.join(self.gen_key(token, depth, max_depth) for token in rule)
+
+    def fuzz(self, key='<start>', max_depth=10):
+        return self.gen_key(key=key, depth=0, max_depth=max_depth)
+
+
 def unify_key(grammar, key):
     return unify_rule(grammar, random.choice(grammar[key])) if key in grammar else [key]
 
@@ -13,11 +65,13 @@ def main(arg):
     with open(arg) as f:
         g = json.load(fp=f)
     k = list(g.keys())[0]
+    lf = LimitFuzzer(g)
     for i in range(10):
-        v = unify_key(g, k)
-        print(repr(''.join(v)))
+        v = lf.fuzz(key=k)
+        # v = unify_key(g, k)
+        print(repr(v))
         print('------')
-        print(''.join(v))
+        print(v)
         print('------')
 
 if __name__ == '__main__':
