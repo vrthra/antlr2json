5cfe642f48467e4d5d4f35e0013962cf441fb0b2
Rahul Gopinath <rahul@gopinath.org>
2020-01-09 16:46:16 +0100

    update

--- src/LexerAdaptor.py
+++ src/LexerAdaptor.py
@@ -18,6 +18,7 @@ class LexerAdaptor(Lexer):
 
     _currentRuleType = Token.INVALID_TYPE
     insideOptionsBlock = False
+    PREQUEL_CONSTRUCT = False
 
     def __init__(self, inp, output):
         Lexer.__init__(self, inp, output)
@@ -70,6 +71,36 @@ class LexerAdaptor(Lexer):
         elif self._type == self.SEMI:  # exit rule def
             self._currentRuleType = Token.INVALID_TYPE
         return Lexer.emit(self)
+
+        # '''
+        # https://github.com/antlr/grammars-v4/commit/5bb7f5243e0a582f819e88775c8e6f20f7839dc7#diff-0a66dbb87735ad1a8e53edf6cdd5227c
+        # public Token emit() {
+        #-        if (_type == ANTLRv4Lexer.ID) {
+        #         if ((_type == ANTLRv4Lexer.OPTIONS || _type == ANTLRv4Lexer.TOKENS || _type == ANTLRv4Lexer.CHANNELS)
+        #                 && _currentRuleType == Token.INVALID_TYPE) { // enter prequel construct ending with an RBRACE
+        #             _currentRuleType = PREQUEL_CONSTRUCT;
+        #         } else if (_type == ANTLRv4Lexer.RBRACE && _currentRuleType == PREQUEL_CONSTRUCT) { // exit prequel construct
+        #             _currentRuleType = Token.INVALID_TYPE;
+        #         } else if (_type == ANTLRv4Lexer.AT && _currentRuleType == Token.INVALID_TYPE) { // enter action
+        #             _currentRuleType = ANTLRv4Lexer.AT;
+        #         } else if (_type == ANTLRv4Lexer.END_ACTION && _currentRuleType == ANTLRv4Lexer.AT) { // exit action
+        #             _currentRuleType = Token.INVALID_TYPE;
+        #         } else if (_type == ANTLRv4Lexer.ID) {
+        #             String firstChar = _input.getText(Interval.of(_tokenStartCharIndex, _tokenStartCharIndex));
+        #             if (Character.isUpperCase(firstChar.charAt(0))) {
+        #                 _type = ANTLRv4Lexer.TOKEN_REF;
+        #             } else {
+        #                 _type = ANTLRv4Lexer.RULE_REF;
+        #             }
+        #             if (_currentRuleType == Token.INVALID_TYPE) { // if outside of rule def
+        #                 _currentRuleType = _type; // set to inside lexer or parser rule
+        #             }
+        #         } else if (_type == ANTLRv4Lexer.SEMI) { // exit rule def
+        #             _currentRuleType = Token.INVALID_TYPE;
+        #         }
+        #         return super.emit();
+        #     }
+        # '''
 
     def inLexerRule(self):
         return self._currentRuleType == self.TOKEN_REF
